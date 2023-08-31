04af37408816cebf733721772653ab19c942755d
Patrick Markowski <PMARKOWS@depaul.edu>
2023-08-30 23:41:49 -0700

    Changed how header looks, utlizing collapsed menu feature of react-bootstrap

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,42 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavDropdown } from 'react-bootstrap';
 
-import {Nav, Navbar, Button, Alert, Breadcrumb, Container} from 'react-bootstrap';
-function Header() {
+function Header(props) {
+  const {isLoggedIn} = props;
 
   return (
-    <Navbar className="bg-dark" expand="lg">
+    <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/" style={{color:"white"}}>RoadTrips</Navbar.Brand>
-        <Navbar.Brand href="/login" style={{color:"white"}}>Logout</Navbar.Brand>
-      </Container>
+          <Navbar.Brand href="/">RoadTrips</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              {isLoggedIn  
+                ?<>
+                <Nav.Link href="/profile">Profile</Nav.Link>
+                <NavDropdown title="Settings" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="/">Change username</NavDropdown.Item>
+                  <NavDropdown.Item href="/">Change password</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="/login">Logout</Nav.Link>
+                </>
+                :<>
+                  <Nav.Link href="/login">Login</Nav.Link>
+                </>
+              }
+
+              
+
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
     </Navbar>
+
+
   )
 }
 
