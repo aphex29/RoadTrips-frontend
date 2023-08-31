81f3d185f901e5e52c9a57c4c7bf9c9a0ca2fbd5
Patrick Markowski <PMARKOWS@depaul.edu>
2023-08-30 23:38:36 -0700

    Utilizing localStorage as well as context to persist user data throughout entire journey

--- src/App.js
+++ src/App.js
@@ -5,13 +5,29 @@ import {Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
 import LoginForm from './components/loginform/LoginForm';
+import LoginFormContainer from './components/loginform/LoginFormContainer';
+import UserProfile from './components/profile/UserProfile';
+import { useUser } from './components/UserContext';
 
 function App() {
   const URL = "http://localhost:8080"
 
-  const [userInfo,setUserInfo] = useState();
+  const {userInfo,setUserInfo} = useUser();
+//const {userInfo,setUserInfo} = useState({});
 
   const [isLoggedIn,setIsLoggedIn] = useState(false);
+
+
+  useEffect(()=>{
+    const user = localStorage.getItem('user');
+    setUserInfo(user);
+  },[]);
+  
+  
+  useEffect(()=>{
+    localStorage.setItem('user', userInfo);
+  },[userInfo]);
+
 
   // const getUsers = async() => {
 
@@ -31,10 +47,11 @@ function App() {
 
     <div className="App">
 
-      <Header/>
+      <Header isLoggedIn={isLoggedIn}/>
       <Routes>
-          <Route path="/" element={<Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} setUserInfo={setUserInfo} userInfo={userInfo}/>}></Route>
-          <Route path="/login" element={<LoginForm/>}></Route>
+          <Route path="/" element={<Home isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />}></Route>
+          <Route path="/login" element={<LoginFormContainer setUserInfo={setUserInfo} setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} userInfo={userInfo}/>}></Route>
+          <Route path="/profile/" element={<UserProfile isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>}></Route>
           <Route path="/createAccount" element={<></>}></Route>
       </Routes>
       
