21733d095b089b0c692fd5cc5c217b0df86e40e4
Patrick Markowski <PMARKOWS@depaul.edu>
2023-08-30 23:39:56 -0700

    Wrapped entire app around UserProvider, to provide global context to subcomponents

--- src/index.js
+++ src/index.js
@@ -4,17 +4,23 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { UserProvider } from './components/UserContext';
+
+
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/*" element={<App />}/>
-    </Routes>
-    </BrowserRouter>
+  <UserProvider>
     
-  </React.StrictMode>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App/>}/>
+      </Routes>
+      </BrowserRouter>
+    
+  </UserProvider>
 );
 
