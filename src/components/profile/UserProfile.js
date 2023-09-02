3c0699ca432db3a7734c3b80505d545e28e0354a
Patrick Markowski <PMARKOWS@depaul.edu>
2023-09-01 23:12:35 -0700

    Added functionality to dynamically display user routes based on their selections

--- src/components/profile/UserProfile.js
+++ src/components/profile/UserProfile.js
@@ -1,31 +1,29 @@
-import {React, useEffect} from 'react'
+import {React, useEffect, useState} from 'react'
 import { Navigate } from 'react-router-dom';
+import axios from 'axios';
 import { useUser } from '../UserContext';
 
 
 function UserProfile() {
-
+  const URL = 'http://localhost:8080/';
 
   const {userInfo,setUserInfo,isLoggedIn, setIsLoggedIn} = useUser();
+  const [routes,setRoutes] = useState(null);
   
+  const listRoutes = userInfo.routes.map(route=>
+    {
+      return <div key={route.id} style={{"flexBasis":"30%"}} >Origin: {route.origin} --&#62; Destination: {route.destination}</div>;
+    });
+
+
 
   return (
-    
-
-    
-    <div>           
-      {userInfo.id}
-      <br/>
-      {userInfo.email}
-      <br/>
-      {userInfo.username}
-      <br/>
-      {userInfo.firstName}
-      <br/>
-      {userInfo.lastName}           
+    <div className="vh-100" style={{"width":"99vw","background":"rgb(61, 61, 61)","color":"white"}}>
+      Here are your currents routes:
+      <div className="d-flex flex-row justify-content-around align-items-center h-100" style={{"flexWrap":"wrap"}}>                    
+          {listRoutes}        
+      </div>
     </div>
-
-
   )
 }
 
