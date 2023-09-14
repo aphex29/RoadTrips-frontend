d46b0c6b5c22c3923ba29702ebb4fe942dceedf4
Patrick Markowski <PMARKOWS@depaul.edu>
2023-09-14 00:01:16 -0700

    Cleaning up codebase, removing unused imports and restructuring code for readability

--- src/components/loginform/LoginFormContainer.js
+++ src/components/loginform/LoginFormContainer.js
@@ -1,40 +1,17 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import LoginForm from './LoginForm';
 import { useUser } from '../UserContext';
 import { Navigate } from 'react-router-dom';
 import axios from 'axios';
 
-
-
-
-function LoginFormContainer(props) {
-
+function LoginFormContainer() {
   const URL = 'http://localhost:8080/';
-
-  const [accountVerified, setAccountVerified] = useState(null)
   const {userInfo,setUserInfo,isLoggedIn,setIsLoggedIn} = useUser();
-
   const[login,setLogin] = useState({
     username:"",
     password:""
   });
 
- 
-  // useEffect(() => {
-  //     const getPerson = async () => {
-  //         let response = await axios.get('http://localhost:8080/api/users/'+login.username)
-  //           .then((response) => response.json())
-  //           .then(result => console.log(result))
-  //           .catch(error => console.log(error));
-  //         console.log(response);
-  //     }
-  //     if (accountVerified){
-  //       getPerson();
-  //     }
-    
-  // }, [accountVerified]);
-  
- 
   const handleSubmit = async(e) =>{
     e.preventDefault();
     let response = axios.post(URL + 'api/v1/verify', login)
@@ -54,8 +31,6 @@ function LoginFormContainer(props) {
     setLogin((prev) => ({...prev, [name]: value}));
   }
 
-
-
   return(
     <div>
       <LoginForm  handleSubmit={handleSubmit}
@@ -63,15 +38,9 @@ function LoginFormContainer(props) {
                   username={login.username} 
                   password={login.password}/>
       {isLoggedIn && <Navigate to={"/profile/"+userInfo.username}/>}
-      <div>No account? Create one <a href="/create" >here</a>.</div>
-      
+      <div>No account? Create one <a href="/create" >here</a>.</div>    
     </div>
   )
-  
 }
 
-
-
-
-
-export default LoginFormContainer
+export default LoginFormContainer;
